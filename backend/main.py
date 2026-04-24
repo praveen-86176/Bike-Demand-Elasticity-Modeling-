918ba2f254c45f6e775db69007750e78d61f7428
Praveen Kumar <praveenk86176@gmail.com>
2026-04-25 02:49:28 +0530

    fix: allow all origins for CORS

--- backend/main.py
+++ backend/main.py
@@ -13,12 +13,8 @@ async def startup():
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000", 
-        "http://127.0.0.1:3000",
-        "https://elasticityai.netlify.app"
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
